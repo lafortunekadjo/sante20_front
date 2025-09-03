4c3647adcdf79922f02d290824989327f2796bce
lafortunekadjo <79406164+lafortunekadjo@users.noreply.github.com>
2025-09-03 17:50:56 +0000

    Update environment.ts

--- src/app/environment.ts
+++ src/app/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
   //apiUrl: 'http://localhost:8080/api',
-  apiUrl:'https://direct-kay-sante-580f1a28.koyeb.app/api'
+  apiUrl:'https://mean-fredelia-mon2-0-279bc751.koyeb.app/api'
 
 };
